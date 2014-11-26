d942f33fd0978e1593518834bd8032ce57380978
Bandedustik <apple@bandedustik-3.local>
2014-11-26 21:12:00 +0300

    added a test class

---
+++ Class.h
@@ -0,0 +1,13 @@
+//
+//  Class.h
+//  Git Test
+//
+//  Created by Bandedustik on 26.11.14.
+//  Copyright (c) 2014 Romanenko Anton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Class : NSObject
+
+@end
---
+++ Class.m
@@ -0,0 +1,13 @@
+//
+//  Class.m
+//  Git Test
+//
+//  Created by Bandedustik on 26.11.14.
+//  Copyright (c) 2014 Romanenko Anton. All rights reserved.
+//
+
+#import "Class.h"
+
+@implementation Class : NSObject
+
+@end
